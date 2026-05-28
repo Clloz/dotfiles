6af927425415dea405d7fb219b4066ec3acc57ae
Clloz <clloz@outlook.com>
2026-05-28 22:39:03 +0800

    chore: set git global proxy, refactor proxy.zsh

--- zsh/.zsh/proxy.zsh
+++ zsh/.zsh/proxy.zsh
@@ -3,13 +3,41 @@
 # clash mihomo
 export PATH="/opt/clash/bin:$PATH"
 
-# proxy list
-export all_proxy="http://127.0.0.1:7890"
-export https_proxy="http://127.0.0.1:7890"
+# 默认静默开启代理
 export http_proxy="http://127.0.0.1:7890"
-alias vproxy="export all_proxy=socks5://127.0.0.1:1090; echo \"Set proxy successful\" "
-alias proxy="export https_proxy=http://127.0.0.1:7890;export http_proxy=http://127.0.0.1:7890;export all_proxy=socks5://127.0.0.1:7890;echo \"Set proxy successfully\" "
-alias testproxy="curl -vv https://www.google.com"
-alias testgithub="curl -vv https://github.com"
-alias unproxy="unset http_proxy;unset https_proxy;unset all_proxy;echo \"Unset proxy successfully\" "
-alias iptest="curl cip.cc"
+export https_proxy="http://127.0.0.1:7890"
+export all_proxy="socks5://127.0.0.1:7890"
+
+export HTTP_PROXY="$http_proxy"
+export HTTPS_PROXY="$https_proxy"
+export ALL_PROXY="$all_proxy"
+
+proxy() {
+  export http_proxy="http://127.0.0.1:7890"
+  export https_proxy="http://127.0.0.1:7890"
+  export all_proxy="socks5://127.0.0.1:7890"
+
+  export HTTP_PROXY="$http_proxy"
+  export HTTPS_PROXY="$https_proxy"
+  export ALL_PROXY="$all_proxy"
+
+  echo "Set proxy successfully"
+}
+
+unproxy() {
+  unset http_proxy https_proxy all_proxy
+  unset HTTP_PROXY HTTPS_PROXY ALL_PROXY
+  echo "Unset proxy successfully"
+}
+
+testproxy() {
+  curl -vv https://www.google.com
+}
+
+testgithub() {
+  curl -vv https://github.com
+}
+
+iptest() {
+  curl cip.cc
+}
