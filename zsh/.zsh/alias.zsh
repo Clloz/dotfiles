5cb29b3163a4af488370965a6a08ea4f4dde4b32
Clloz <clloz@outlook.com>
2026-05-30 14:41:32 +0800

    chore: update alias, update brew config

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -79,7 +79,25 @@ t() {
   local name
   name=$(basename "$dir" | tr ' .:' '___')
 
-  tmux new-session -A -s "$name" -c "$dir"
+  if tmux has-session -t "$name" 2>/dev/null; then
+    if [[ -n "$TMUX" ]]; then
+      tmux switch-client -t "$name"
+    else
+      tmux attach-session -t "$name"
+    fi
+  else
+    if [[ -n "$TMUX" ]]; then
+      tmux new-session -d -s "$name" -c "$dir"
+      tmux switch-client -t "$name"
+    else
+      tmux new-session -s "$name" -c "$dir"
+    fi
+  fi
+}
+ts() {
+    local session
+    session=$(tmux list-sessions -F '#S' | fzf) || return
+    tmux switch-client -t "$session"
 }
 
 # ---------------------------------------------------------
--- zsh/.zsh/brew.zsh
+++ zsh/.zsh/brew.zsh
@@ -1,22 +1,26 @@
 #!/bin/zsh
-# homebrew zsh config
-# 添加 homebrew 到环境变量
-#export PATH="$HOME/bin:/usr/local/bin:/opt/homebrew/bin:$PATH"
-#export PATH="/opt/homebrew/bin:$PATH"
-#export PATH="/opt/homebrew/sbin:$PATH"
-# ---- Homebrew (ONCE, correct way) ----
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# Homebrew config
 
-# 设置 homebrew 清华源
+# macOS Apple Silicon: /opt/homebrew/bin/brew
+# macOS Intel:         /usr/local/bin/brew
+# Linuxbrew:           /home/linuxbrew/.linuxbrew/bin/brew
+
+if [[ -x /opt/homebrew/bin/brew ]]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  alias brewdir="cd /opt/homebrew"
+elif [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  alias brewdir="cd /home/linuxbrew/.linuxbrew"
+elif [[ -x /usr/local/bin/brew ]]; then
+  eval "$(/usr/local/bin/brew shellenv)"
+  alias brewdir="cd /usr/local"
+fi
+
+# Homebrew mirrors
 export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
 export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
 #export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
 #export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
 #export HOMEBREW_PIP_INDEX_URL="https://pypi.tuna.tsinghua.edu.cn/simple"
 #export HOMEBREW_NO_INSTALL_FROM_API=1
-
-#alias
-alias brewdir="cd /opt/homebrew"
-
-
 
